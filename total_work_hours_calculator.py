d7d55674ae2a7f624d9488826f5c385e766fc91b
megahomyak <g.megahomyak@gmail.com>
2022-10-01 19:46:16 +0500

    Removed repeating months

--- total_work_hours_calculator.py
+++ total_work_hours_calculator.py
@@ -2,6 +2,13 @@ import bs4
 import sys
 from dataclasses import dataclass
 from typing import List
+from collections import OrderedDict
+
+
+@dataclass
+class MonthDescription:
+    days_amount: int
+    skipped_lessons_amount: int
 
 
 @dataclass
@@ -13,16 +20,15 @@ class Month:
 
 @dataclass
 class Report:
-    lesson_name: str
     workers_amount: int
     months: List[Month]
 
 
-def get_reports(file_path) -> List[Report]:
+def get_report(file_path) -> Report:
     document = open(file_path, encoding="utf-8").read()
     soup = bs4.BeautifulSoup(document, "html.parser")
     tables = soup.find_all("table")
-    reports = []
+    month_descriptions = OrderedDict()
     for table in tables:
         title = table.find_previous("div").text
         if title.split(":")[0] == "Предмет":  # We need this table
@@ -37,30 +43,32 @@ def get_reports(file_path) -> List[Report]:
             )
             workers_amount = len(rows) - 2  # Rows_amount - headings_amount
             lesson_name = title.split(":")[1].strip()
-            processed_months = []
             index_offset = 2  # Skipping the number and name columns
             for month in months:
+                month_name = month.text
+                month_description = month_descriptions.setdefault(
+                    month_name, MonthDescription(0, 0)
+                )
                 days_amount = int(month.attrs["colspan"])
+                month_description.days_amount += days_amount
                 skipped_lessons_amount = 0
                 for row in rows[2:]:  # Cutting the heading
                     row = row.find_all("td")
                     for i in range(days_amount):
                         cell_text = row[index_offset + i].text.strip()
                         if cell_text == "Н":
-                            skipped_lessons_amount += 1
+                            month_description.skipped_lessons_amount += 1
                 index_offset += days_amount
-                processed_months.append(
-                    Month(month.text, days_amount, skipped_lessons_amount)
-                )
-            reports.append(Report(lesson_name, workers_amount, processed_months))
-    return reports
+    return Report(workers_amount, [
+        Month(name, description.days_amount, description.skipped_lessons_amount)
+        for name, description in month_descriptions.items()
+    ])
 
 
-reports = get_reports(sys.argv[1])
-for report in reports:
-    for month in report.months:
-        taught_lessons_number = (
-            month.days_amount * report.workers_amount
-            - month.skipped_lessons_amount
-        )
-        print(f"{month.name}: {taught_lessons_number} отработанных дней")
+report = get_report(sys.argv[1])
+for month in report.months:
+    taught_lessons_number = (
+        month.days_amount * report.workers_amount
+        - month.skipped_lessons_amount
+    )
+    print(f"{month.name}: {taught_lessons_number} отработанных дней")
